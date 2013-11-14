95e2046b8e5e7cfaef1ace3b0765013093824aee
RJ Dabbar <rjdabbar@gmail.com>
2013-11-13 19:55:02 -0500

    getting ready for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,6 @@ class GamesController < ApplicationController
   def index
   end
 
-  def debug
-    @not_special = params["not_special"]
-  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 GamingApp::Application.routes.draw do
   get "games", to: 'games#index'
-  get "debug/:not_special", to: 'games#debug'
+  
 end
