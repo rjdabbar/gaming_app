b3088cef2ac855cc0c9c05058f982a02d65ed20b
RJ Dabbar <rjdabbar@gmail.com>
2013-11-13 21:03:51 -0500

    working rock paper scissors

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,5 +20,23 @@ class GamesController < ApplicationController
 
   end
 
+  def rock_paper_scissors
+  end
+
+  def rock_paper_scissors_play
+    @app_throw = ["rock", "paper", "scissors"].sample
+    @user_throw = params[:throw]
+
+    if @app_throw == @user_throw
+      @status = "You tied!"
+    elsif (@app_throw == "rock" && @user_throw == "paper") || (@app_throw == "paper" && @user_throw == "scissors") || (@app_throw == "scissors" && @user_throw == "rock")
+      
+      @status = "You Win"
+      else
+        @status = "You Lost"
+    end
+  end
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ GamingApp::Application.routes.draw do
   get "games", to: 'games#index'
   get "games/secret_number", to: 'games#secret_number'
   get "games/secret_number/:guess", to: 'games#secret_number_play'
-
+  get "games/rock_paper_scissors", to: 'games#rock_paper_scissors'
+  get "games/rock_paper_scissors/:throw", to: 'games#rock_paper_scissors_play'
 end
