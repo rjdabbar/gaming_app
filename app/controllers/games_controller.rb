1bdd5a038082bd7546f8f72016e1a55501d88e60
RJ Dabbar <rjdabbar@gmail.com>
2013-11-13 20:48:41 -0500

    working secret num game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,24 @@
 class GamesController < ApplicationController
+ 
+
   def index
+    @games = ["Secret Number"]
   end
 
+  def secret_number
+
+  end
+
+  def secret_number_play
+    @secret_number = rand(1..5)
+    @guess = params[:guess].to_i
+    if @secret_number == @guess
+      render 'win'
+    else
+      render 'lose'
+    end
+
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 GamingApp::Application.routes.draw do
+  root "games#index"
   get "games", to: 'games#index'
-  
+  get "games/secret_number", to: 'games#secret_number'
+  get "games/secret_number/:guess", to: 'games#secret_number_play'
+
 end
