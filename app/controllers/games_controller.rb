64f488cf85517f8bd8c0e2fb2cefaf01355b239b
RJ Dabbar <rjdabbar@gmail.com>
2013-11-13 21:24:30 -0500

    hashed rps version

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,17 +24,30 @@ class GamesController < ApplicationController
   end
 
   def rock_paper_scissors_play
+
+    rps = {"rock" => "paper", "paper" => "scissors", "scissors" => "rock"}
     @app_throw = ["rock", "paper", "scissors"].sample
     @user_throw = params[:throw]
+    @user_loses_to = rps[@user_throw]
 
     if @app_throw == @user_throw
-      @status = "You tied!"
-    elsif (@app_throw == "rock" && @user_throw == "paper") || (@app_throw == "paper" && @user_throw == "scissors") || (@app_throw == "scissors" && @user_throw == "rock")
-      
-      @status = "You Win"
-      else
-        @status = "You Lost"
+      @status = "Tie"
+    elsif @app_throw == @user_loses_to
+      @status = "You Lost"
+    else
+      @status ="You win"
     end
+
+      
+
+    # if @app_throw == @user_throw
+    #   @status = "You tied!"
+    # elsif (@app_throw == "rock" && @user_throw == "paper") || (@app_throw == "paper" && @user_throw == "scissors") || (@app_throw == "scissors" && @user_throw == "rock")
+      
+    #   @status = "You Win"
+    #   else
+    #     @status = "You Lost"
+    # end
   end
 
 
